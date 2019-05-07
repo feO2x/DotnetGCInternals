2869b8660e37ccfa15ea3644ce0dea9b0ffd28b5
Kenny Pflug <kenny.pflug@live.de>
2019-05-07 23:02:03 +0200

    added ExcelParsing performance benchmark

---
+++ Code/DotnetGCInternals.Performance/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace DotnetGCInternals.Performance
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, CreateDefaultConfig());
+        }
+
+        private static IConfig CreateDefaultConfig() =>
+            DefaultConfig.Instance
+                         .With(Job.Core)
+                         .With(MemoryDiagnoser.Default);
+    }
+}
